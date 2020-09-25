3a59cd18e0c92b92b37c9e7a207d1338c5fb1a04
m3y <ma3ya.ozw+github@gmail.com>
2020-09-25 20:37:56 +0900

    add clap part and initialize module

---
+++ src/chkapr/github_repository.rs
@@ -0,0 +1,11 @@
+pub fn query(
+    target: String,
+    repository: String,
+    github_token: String,
+    organization: String,
+    approvable_team: String,
+    base_ref: String,
+    head_ref: String,
+) -> String {
+    "github_repository::query".to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+pub mod chkapr;
+
+use clap::{crate_authors, crate_description, crate_name, crate_version, ArgSettings, Clap};
+
+use crate::chkapr::github_repository;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = crate_name!(),
+    version = crate_version!(),
+    author = crate_authors!(),
+    about = crate_description!()
+)]
+struct Opts {
+    #[clap(
+        long,
+        env = "GITHUB_TOKEN",
+        setting = ArgSettings::HideEnvValues,
+        about = "github token"
+    )]
+    github_token: String,
+    #[clap(long, env = "TARGET", about = "target tag and label.")]
+    target: String,
+    #[clap(long, env = "TARGET_REPO", about = "target repository name.")]
+    repository: String,
+    #[clap(
+        long,
+        env = "BASE_REF",
+        default_value = "pay2release",
+        about = "base ref name."
+    )]
+    base_ref: String,
+    #[clap(
+        long,
+        env = "HEAD_REF",
+        default_value = "master",
+        about = "head ref name."
+    )]
+    head_ref: String,
+    #[clap(
+        long,
+        env = "ORGANIZATION",
+        default_value = "Pay-Baymax",
+        about = "organization name."
+    )]
+    organization: String,
+    #[clap(
+        long,
+        env = "APPROVABLE_TEAM",
+        default_value = "tech-leads",
+        about = "an approvable team."
+    )]
+    approvable_team: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+    println!("{:#?}", opts);
+    let result = github_repository::query(
+        opts.target,
+        opts.repository,
+        opts.github_token,
+        opts.organization,
+        opts.approvable_team,
+        opts.base_ref,
+        opts.head_ref,
+    );
+
+    println!("{}", result);
 }
